1016528dce8b871a507ca85ccb337dcfda8104ed
Sujan29k <kharelsuzan456@gmail.com>
2025-09-23 15:58:52 +0545

    made the app todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,161 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TodoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// Model for Todo
+class Todo {
+  String title;
+  bool isDone;
 
-  // This widget is the root of your application.
+  Todo({required this.title, this.isDone = false});
+
+  // Convert Todo to Map for storage
+  Map<String, dynamic> toMap() {
+    return {'title': title, 'isDone': isDone};
+  }
+
+  // Convert Map back to Todo
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(title: map['title'], isDone: map['isDone']);
+  }
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Better Todo App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const TodoHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TodoHomePage extends StatefulWidget {
+  const TodoHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TodoHomePage> createState() => _TodoHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoHomePageState extends State<TodoHomePage> {
+  final List<Todo> _todos = [];
+  final TextEditingController _controller = TextEditingController();
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _loadTodos(); // Load saved tasks on start
+  }
+
+  // Load todos from SharedPreferences
+  Future<void> _loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? todosString = prefs.getString('todos');
+    if (todosString != null) {
+      final List decoded = jsonDecode(todosString);
+      setState(() {
+        _todos.clear();
+        _todos.addAll(decoded.map((e) => Todo.fromMap(e)));
+      });
+    }
+  }
+
+  // Save todos to SharedPreferences
+  Future<void> _saveTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String encoded = jsonEncode(
+      _todos.map((todo) => todo.toMap()).toList(),
+    );
+    await prefs.setString('todos', encoded);
+  }
+
+  void _addTodo() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _todos.add(Todo(title: _controller.text));
+        _controller.clear();
+        _saveTodos();
+      });
+    }
+  }
+
+  void _removeTodoAt(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _todos.removeAt(index);
+      _saveTodos();
+    });
+  }
+
+  void _toggleTodoStatus(int index) {
+    setState(() {
+      _todos[index].isDone = !_todos[index].isDone;
+      _saveTodos();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button thi  :'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: const Text('Better Todo App')),
+      body: Column(
+        children: [
+          // Input Row
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      labelText: 'Enter task',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(onPressed: _addTodo, child: const Text('Add')),
+              ],
             ),
-          ],
-        ),
+          ),
+          // Pending tasks
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                final todo = _todos[index];
+                return ListTile(
+                  leading: Checkbox(
+                    value: todo.isDone,
+                    onChanged: (_) => _toggleTodoStatus(index),
+                  ),
+                  title: Text(
+                    todo.title,
+                    style: TextStyle(
+                      decoration: todo.isDone
+                          ? TextDecoration.lineThrough
+                          : null,
+                      color: todo.isDone ? Colors.grey : Colors.black,
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _removeTodoAt(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,107 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:my_first_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('Todo App Tests', () {
+    testWidgets('Todo app has title and input field', (
+      WidgetTester tester,
+    ) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const TodoApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Verify that the app title is displayed
+      expect(find.text('Enhanced Todo App'), findsWidgets);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Verify that input field is present
+      expect(find.byType(TextField), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Verify that Add button is present
+      expect(find.text('Add'), findsOneWidget);
+    });
+
+    testWidgets('Can add a todo item', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const TodoApp());
+
+      // Enter text into the text field
+      await tester.enterText(find.byType(TextField), 'Test todo item');
+
+      // Tap the Add button
+      await tester.tap(find.text('Add'));
+      await tester.pump();
+
+      // Verify that the todo item appears in the list
+      expect(find.text('Test todo item'), findsOneWidget);
+
+      // Verify that there's a checkbox for the new item
+      expect(find.byType(Checkbox), findsOneWidget);
+
+      // Verify that there's a delete button for the new item
+      expect(find.byIcon(Icons.delete), findsOneWidget);
+    });
+
+    testWidgets('Can toggle todo completion status', (
+      WidgetTester tester,
+    ) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const TodoApp());
+
+      // Add a todo item
+      await tester.enterText(find.byType(TextField), 'Test todo item');
+      await tester.tap(find.text('Add'));
+      await tester.pump();
+
+      // Find the checkbox and verify it's unchecked
+      final checkbox = find.byType(Checkbox);
+      expect(tester.widget<Checkbox>(checkbox).value, false);
+
+      // Tap the checkbox to mark as completed
+      await tester.tap(checkbox);
+      await tester.pump();
+
+      // Verify the checkbox is now checked
+      expect(tester.widget<Checkbox>(checkbox).value, true);
+    });
+
+    testWidgets('Can delete a todo item', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const TodoApp());
+
+      // Add a todo item
+      await tester.enterText(find.byType(TextField), 'Test todo item');
+      await tester.tap(find.text('Add'));
+      await tester.pump();
+
+      // Verify the item exists
+      expect(find.text('Test todo item'), findsOneWidget);
+
+      // Tap the delete button
+      await tester.tap(find.byIcon(Icons.delete));
+      await tester.pump();
+
+      // Verify the item is deleted
+      expect(find.text('Test todo item'), findsNothing);
+    });
+
+    testWidgets('Text field clears after adding todo', (
+      WidgetTester tester,
+    ) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const TodoApp());
+
+      // Enter text into the text field
+      await tester.enterText(find.byType(TextField), 'Test todo item');
+
+      // Tap the Add button
+      await tester.tap(find.text('Add'));
+      await tester.pump();
+
+      // Verify that the text field is cleared
+      expect(find.text('Test todo item'), findsOneWidget); // In the list
+      final textField = tester.widget<TextField>(find.byType(TextField));
+      expect(textField.controller?.text, isEmpty);
+    });
   });
 }
